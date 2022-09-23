db7863c2c7dbd3d58ad4b5a88b16ea51467b1a38
Nolan McCloskey <nolan@Nolans-Air.home>
2022-09-23 18:38:31 -0400

    This is the first commit

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Knight, Food } from "./Entities.js";
+import { GameEngine } from "react-native-game-engine";
+
+
+
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GameEngine
+    entities = {{
+      1: {position: [100, 100], renderer: <Knight /> }
+    }}>
+      
+    </GameEngine>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
