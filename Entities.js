5d79bed87899b37ebddb0ae68cec55456ae16d40
nolan-3 <agreattofutaxpayer@gmail.com>
2022-09-23 19:03:01 -0400

    second message

--- Entities.js
+++ Entities.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-
+//main player character
 const Knight = (props) => {
   return (
     <View>
@@ -11,7 +11,8 @@ const Knight = (props) => {
   </View>
   );
 }
-//test
+
+
 
 const Food = () => {
     return (
