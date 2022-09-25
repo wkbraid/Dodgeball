8146752138b6e1c6ac2eddab4cf7066743914c20
nolan-3 <agreattofutaxpayer@gmail.com>
2022-09-25 14:53:57 -0400

    Several bouncing projectiles

--- Entities.js
+++ Entities.js
@@ -1,46 +1,102 @@
-import React from 'react';
+import React, { Component, } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { Dimensions } from 'react-native';
+
+const MAXwidth = Dimensions.get('window').width;
+const MAXheight = Dimensions.get('window').height;
+//if we want the screen to turn in game then this should be 
+//called in the loop
 
 //main player character
-const Knight = (props) => {
-  console.log("knight runs")
-  return (
-      <View style=
-    {{ height: 100, width: 200, backgroundColor: "blue", 
-      left: props.position[0], top: props.position[1] }} />
-  );
-}
-//test
-
-const Brick = (props) => {
-  console.log("Brick runs") //... every time the game updates
-  //let position_x = props.position[0]
-  //let position_y = props.position[1]
-
-  //position_x += props.velocity[0]
-  //position_y += props.velocity[1]
-return(
-  <View
-  style = {{height: 50, width: 50, backgroundColor: "red",
-             }}
-             //left: Math.floor(Math.random()*500), top: 50,
-  />
-)
+class Knight{
+  constructor(){
+    this.height = 200;
+    this.width = 200;
+    this.backgroundColor = "blue";
+    this.x = 400;
+    this.y = 400;
+    this.velocity = [0,0];
+    //console.log("Knight runs");
 
 }
+update(){
+  //we call update on every entity so we something to run for knight
+}
+}
 
-const Circle = (props) => {
-    return (
-      <View
-      style = {{ height: 100, width: 100, 
-              backgroundColor: "red", borderRadius: 100/2,
-              left: props.position[0], top: props.position[1]}}
-                  />
+class Brick {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.backgroundColor = 'red';
+    this.height = 75;
+    this.width = 75;
 
+    let randomNum = Math.random()*3
 
-    );
+    this.velocityX = randomNum;
+    this.velocityY = 3 - randomNum;
+    //console.log("Brick constructor runs")
   }
 
-  
+  update() {
+    //console.log("Brick update runs")
+    
+    //If the projectiles start at 0 they get stuck
+    //Bouncing:
+    if(this.x >= MAXheight){
+      this.velocityX = (this.velocityX * -1)
+      this.velocityY = (this.velocityY * -1)
+    }
+    if(this.x <= 0){
+      this.velocityX = (this.velocityX * -1)
+      this.velocityY = (this.velocityY * -1)
+    }
+    if(this.y >= MAXheight){
+      this.velocityX = (this.velocityX * -1)
+      this.velocityY = (this.velocityY * -1)
+    }
+    if(this.y <= 0){
+      this.velocityX = (this.velocityX * -1)
+      this.velocityY = (this.velocityY * -1)
+    }
 
-export {Knight, Circle, Brick};
+    this.x += this.velocityX;
+    this.y += this.velocityY;
+
+    
+
+  }
+}
+
+class Circle {
+  constructor(){
+    this.backgroundColor = "red";
+    this.radius = 100/2;
+    //borderRadius is the thing to change
+    //how to make Create accept radius, if statement could suffice, slow?
+    this.x = Math.floor(Math.random()*500);
+    this.y = 0;
+    this.velocityX = 1;
+    this.velocityY = 1;
+    }
+  
+  update() {
+    //console.log("Circle update runs");
+    this.x += this.velocityX;
+    this.y += this.velocityY;
+    //Move(this)
+  } 
+}
+  /*let x = entity.velocity[0]
+  let y = entity.velocity[1]
+
+  entity.left += x 
+  entity.top += y 
+
+  return entity*/
+  // code for moving projectiles CAN go back in Gameloop, 
+  // if that makes it faster or something
+
+
+export {Knight, Circle, Brick,};
