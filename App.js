4de63d0ea4825e204ab0e3956f5604c52e904ac0
nolan-3 <agreattofutaxpayer@gmail.com>
2022-09-24 20:20:34 -0400

    code not using classes that doesn't work

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Knight, Food } from "./Entities.js";
+import { Knight, Circle, Brick } from "./Entities.js";
 import { GameEngine } from "react-native-game-engine";
 
 
+function Gameloop(entities, { touches })  {
+  //components = []
+  for(let i = 1; i <= 1; i++){
+  let entity = entities[i] 
+  move(entity)
+ }
+return entities
+}
+  
+
+function move(entity){
+  
+  let x = entity.velocity[0]
+  let y = entity.velocity[1]
+
+  entity.position[0] += x 
+  entity.position[1] += y 
+  // code for moving projectiles CAN go back in Gameloop, 
+  // if that makes it faster or something
+}
+    
+  
+function collision(entities) {
+
+}
+
+//what is 0 indexed, and what is not?
 
 
+function createProjectile(){
+  <Brick/>
+}
+
+var ents = {}
+  //Knight and Brick run every time the game updates that is not good
+//1: {position: [500, 250], velocity: [0,0], renderer: <Knight />}
+
+  for (let i = 1; i <= 3; i++){
+    //how to add a velocity to these entities
+    ents[i] = ({position:[0,0], velocity: [2,2], renderer: <Brick/>})
+    console.log(i)
+    //Math.floor(Math.random()*10)
+  } 
 
 export default function App() {
 
+  //Thank you Will, ok possibly not thank you
+
   return (
     <GameEngine
-    entities = {{
-      1: {position: [100, 100], renderer: <Knight /> }
-    }}>
-      
+    systems = {[Gameloop]}
+    var entities = {ents}>
     </GameEngine>
   );
 }
--- Entities.js
+++ Entities.js
@@ -3,21 +3,44 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 
 //main player character
 const Knight = (props) => {
+  console.log("knight runs")
   return (
-    <View>
       <View style=
-    {{ height: 100, width: 100, backgroundColor: "blue", 
+    {{ height: 100, width: 200, backgroundColor: "blue", 
       left: props.position[0], top: props.position[1] }} />
-  </View>
   );
 }
 //test
 
+const Brick = (props) => {
+  console.log("Brick runs") //... every time the game updates
+  //let position_x = props.position[0]
+  //let position_y = props.position[1]
 
-const Food = () => {
+  //position_x += props.velocity[0]
+  //position_y += props.velocity[1]
+return(
+  <View
+  style = {{height: 50, width: 50, backgroundColor: "red",
+             }}
+             //left: Math.floor(Math.random()*500), top: 50,
+  />
+)
+
+}
+
+const Circle = (props) => {
     return (
-      <Text>Hello, I am your food!</Text>
+      <View
+      style = {{ height: 100, width: 100, 
+              backgroundColor: "red", borderRadius: 100/2,
+              left: props.position[0], top: props.position[1]}}
+                  />
+
+
     );
   }
 
-export {Knight, Food,};
+  
+
+export {Knight, Circle, Brick};
