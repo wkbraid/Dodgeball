ab2a1c62b0a0fad3c95646138f676aa2bbe6ec30
nolan-3 <agreattofutaxpayer@gmail.com>
2022-09-25 14:53:57 -0400

    Several bouncing projectiles

--- App.js
+++ App.js
@@ -1,52 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Knight, Circle, Brick } from "./Entities.js";
+import { Knight, Circle, Brick, } from "./Entities.js";
 import { GameEngine } from "react-native-game-engine";
 
 
 function Gameloop(entities, { touches })  {
-  //components = []
-  for(let i = 1; i <= 1; i++){
-  let entity = entities[i] 
-  move(entity)
- }
+  for (let id in entities) {
+    entities[id].core.update();
+  }
 return entities
 }
-  
 
-function move(entity){
-  
-  let x = entity.velocity[0]
-  let y = entity.velocity[1]
-
-  entity.position[0] += x 
-  entity.position[1] += y 
-  // code for moving projectiles CAN go back in Gameloop, 
-  // if that makes it faster or something
+function Create(props){
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        width: props.core.width,
+        height: props.core.height,
+        left: props.core.x,
+        top: props.core.y,
+        backgroundColor: props.core.backgroundColor,
+      }}
+    />
+  );
 }
+  
+
+
     
   
 function collision(entities) {
 
 }
 
-//what is 0 indexed, and what is not?
 
 
-function createProjectile(){
-  <Brick/>
-}
 
-var ents = {}
+
+
+var ents = {
+  knight: { core: new Knight(), renderer: <Create /> },
+};
+// Knight: { core: new Knight(), renderer: <Create />}
   //Knight and Brick run every time the game updates that is not good
-//1: {position: [500, 250], velocity: [0,0], renderer: <Knight />}
 
-  for (let i = 1; i <= 3; i++){
-    //how to add a velocity to these entities
-    ents[i] = ({position:[0,0], velocity: [2,2], renderer: <Brick/>})
-    console.log(i)
-    //Math.floor(Math.random()*10)
-  } 
+  for (let i = 0; i < 10; i++) {
+    let random = Math.floor(Math.random()*500) + 75
+    ents[i] = { core: new Brick(random, 15), renderer: <Create /> };
+  }
 
 export default function App() {
 
