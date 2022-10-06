d60cf65212df82d68fe1f3a95072e9fc4b341249
nolan-3 <agreattofutaxpayer@gmail.com>
2022-10-05 21:01:19 -0400

    added, at least visually, the joystick, seems like it should be easy enough to use for mobile, however getting to the point where I can test it on mobile might be a bit more difficult.

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { Knight, Circle, Brick, Counter, Move, Knight2, MoveKnight } from "./Entities.js";
 import { GameEngine } from "react-native-game-engine";
 import { useEffect } from 'react';
 import { useOnKeyPress } from "./Key.js"
+import AxisPad from 'react-native-axis-pad';
 
-
+const MAXwidth = Dimensions.get('window').width;
+const MAXheight = Dimensions.get('window').height;
 
 function Gameloop(entities, { touches })  {
 
@@ -48,6 +50,20 @@ export default function App() {
     var entities = {ents}>
     <Knight2/>
     
+
+  <View style={{left: MAXwidth/100, top: 3*MAXheight/5, backgroundcolor: "blue"}}>  
+<AxisPad
+    resetOnRelease={true}
+    autoCenter={true}
+    onValue={({ x, y }) => {
+        // values are between -1 and 1
+        // here insert a call to a function to move the knight entity
+        // momentum would be a good thing to start thinking about
+        console.log(x, y);
+    }}>
+    <Text>Text :( </Text>
+</AxisPad>
+</View>
     
     </GameEngine>
   );
