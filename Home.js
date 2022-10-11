2b4c18048648d653df50c55d930da6bd68a97620
nolan-3 <agreattofutaxpayer@gmail.com>
2022-10-10 21:13:01 -0400

    changed the creation of entities from loops to functions, added a new type of projectile, I modeled it off a grenade, it uses a timer to determine when to explode

---
+++ Home.js
@@ -0,0 +1,21 @@
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+export default function Home({ navigation }) {
+    return (
+        <View>
+        <Button style={styles.button} title="Play" onPress={() => navigation.navigate("Dodge")}/> 
+        </View>
+    )  
+}
+const styles = StyleSheet.create({
+    button: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      borderRadius: 4,
+      elevation: 3,
+      backgroundColor: 'black',
+    }},)
