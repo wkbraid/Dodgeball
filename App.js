1c1202e93e818cf9327d361cc5362109bfff2638
nolan-3 <agreattofutaxpayer@gmail.com>
2022-11-05 22:52:12 -0400

    deleted references to results.js :)

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import DodgeBallGame from "./Dodge.js";
 import Home from "./Home.js";
-import Results from "./Results"
 import Battle from "./Battle.js"
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -26,11 +25,6 @@ export default function App() {
           name="Dodge"
           //children={() => (<DodgeBall/>)}
           component={DodgeBallGame}
-          options={{headerShown: false}}
-        />
-         <Stack.Screen 
-          name="Results"
-          component={Results}
           options={{headerShown: false}}
         />
       </Stack.Navigator>
